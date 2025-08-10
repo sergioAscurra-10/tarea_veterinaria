9f50b3298c6cace06fb3f6b3982353e5cd9a4ad2
sergio <sebastianperez.27102005@gmail.com>
2025-08-10 16:45:52 -0500

    Estructura del proyecto inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tarea3_veterinaria.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
