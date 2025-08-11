ac59f0b16a9ef0ee4b82b8f3150c0aed3a4065ed
sergio <sebastianperez.27102005@gmail.com>
2025-08-10 20:55:50 -0500

    Mensaje de exito

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,12 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult AgregarCarrito()
+    {
+        TempData["mensaje2"] = "Felicidades tu producto se ha agregado correctamente";
+        return RedirectToAction("index");
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
