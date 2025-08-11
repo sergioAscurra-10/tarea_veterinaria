4d117b88b5ed248b4f3a39ebdd7c2145b2652829
sergio <sebastianperez.27102005@gmail.com>
2025-08-10 22:39:48 -0500

    nuevos cambios

--- obj/Debug/net9.0/tarea3_veterinaria.AssemblyInfo.cs
+++ obj/Debug/net9.0/tarea3_veterinaria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tarea3_veterinaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fcfe2f317af5f5ea84770cae856f7937ea1be9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9555af3b2cd83eb404fb7b09c4ca23907cbf8cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("tarea3_veterinaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tarea3_veterinaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
